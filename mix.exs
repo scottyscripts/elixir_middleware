173ef5bc0c3a98b0f2fa8430744d5c9ce09cff3c
Scott Davidson <sdavidson140@gmail.com>
2017-11-21 14:50:30 -0500

    invoke GithubWebhookMiddleware module when app starts
    next: define start method in GithubWebhookMiddleware module

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule GithubWebhookMiddleware.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {GithubWebhookMiddleware, []},
     ]
   end
 
