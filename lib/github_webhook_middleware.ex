e58d459730846a347d6ee2aa079d9cce6ac982d2
Scott Davidson <sdavidson140@gmail.com>
2017-11-21 15:07:48 -0500

    annotate GithubWebhookMiddleware module

--- lib/github_webhook_middleware.ex
+++ lib/github_webhook_middleware.ex
@@ -1,4 +1,5 @@
 defmodule GithubWebhookMiddleware do
+  # module for working with applications and defining application callbacks
   use Application
 
   def hello do
@@ -6,10 +7,23 @@ defmodule GithubWebhookMiddleware do
   end
 
   def start(_type, _args) do
-    children = []
+    # new syntax as of Elixir 1.5 (no longer use Supervisor.spec)
+    children = [
+      # use a tuple instead of an atom: to configure children
+    ]
 
+    # name: name of Supervisor
+    # QUESTION: should I be registering a name for the Supervisor
     opts = [strategy: :one_for_one]
 
+    # strategy: options (restart strategies available to Supervisors)
+    #  :one_for_one - Only restart the failed child process
+    #  :one_for_all - Restart all child processes in event of a failure
+    #  :rest_for_one - Restart the failed process and any process started after it
+    #  :simple_one_for_one - best for dynamically attached children
+
+    # starts supervisor and children, defining strategy for managing child processes
+    # calls child_spec/1 function on given modules
     Supervisor.start_link(children, opts)
   end
 end
